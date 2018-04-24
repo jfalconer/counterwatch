d66b61cd19276193ee33c9fc0c486f341cca0e20
Joel Falconer <joel.falconer@gmail.com>
2018-04-24 18:20:40 +1000

    Fetch battletag data

--- js/scripts.js
+++ js/scripts.js
@@ -11,9 +11,13 @@ const dqsa = (query) => document.querySelectorAll(query);
     dqsa('.right-heroes .hero-item').forEach(clickRightHero => clickRightHero.addEventListener('click', setRightHero));
   });
 
+  document.addEventListener('DOMContentLoaded', function () {
+    dqsa('#matchem-button').forEach(clickMatchem => clickMatchem.addEventListener('click', getBattletagData));
+  });
+
 let leftHero = "";
 let rightHero = "";
-dqs('#info-pane').innerHTML = `Select one hero for hero advice, or two for matchup advice.`;
+dqs('#info-pane').innerHTML = `Select one hero for hero advice, or two for matchup advice. Battletags are optional.`;
 
 function setLeftHero (e) {
   let currentHero = e.target.dataset.hero
@@ -42,13 +46,13 @@ function setRightHero (e) {
 };
 
 function buildContent () {
-        if (leftHero && rightHero) {
-          getMatchupData();
-        } else if (leftHero || rightHero) {
-          getHeroData();
-        } else {
-          dqs('#info-pane').innerHTML = `Select one hero for hero advice, or two for matchup advice.`;
-        }
+  if (leftHero && rightHero) {
+    getMatchupData();
+  } else if (leftHero || rightHero) {
+    getHeroData();
+  } else {
+    dqs('#info-pane').innerHTML = `Select one hero for hero advice, or two for matchup advice. Battletags are optional.`;
+  }
 };
 
 function getMatchupData() {
@@ -59,6 +63,36 @@ function getHeroData() {
   dqs('#info-pane').innerHTML = `Only one hero, ${leftHero || rightHero}, set\n` +
                                 `${heroMatchupInfo[leftHero || rightHero].self}`;
 };
+
+let leftBattletagData = {};
+let rightBattletagData = {};
+
+async function getBattletagData(e) {
+  let leftBattletag = document.getElementById('left-battletag').value.replace("#", "-");
+  let rightBattletag = document.getElementById('right-battletag').value.replace("#", "-");
+  console.log(`${leftBattletag} + ${rightBattletag}`);
+
+  if (leftBattletag && rightBattletag) {
+    leftBattletagData = await loadBattletag(leftBattletag); console.log(leftBattletagData);
+    rightBattletagData = await loadBattletag(rightBattletag); console.log(rightBattletagData);
+  } else if (leftBattletag && !rightBattletag) {
+    leftBattletagData = await loadBattletag(leftBattletag); console.log(leftBattletagData);
+  } else if (!leftBattletag && rightBattletag) {
+    rightBattletagData = await loadBattletag(rightBattletag); console.log(rightBattletagData);
+  }
+}
+
+function loadBattletag(battletag) {
+  return axios.get(`https://owapi.net/api/v3/u/${battletag}/blob`)
+  .then(function (response) {
+    console.log(response);
+    return response.data;
+  })
+  .catch(function (error) {
+    console.log(error);
+    dqs('#info-pane').innerHTML = `Your request for the battletag ${battletag.replace("-", "#")} failed. Make sure the battletag is correct or clear the field.`;
+  });
+}
 
 let heroMatchupInfo = {
   doomfist: {
